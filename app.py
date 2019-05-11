9db90f4f79526d56a349292cd8bdbb0de9cbec5a
eliasdabbas <eliasdabbas@gmail.com>
2019-05-11 03:12:13 +0200

    Use boostrap

--- app.py
+++ app.py
@@ -1,35 +1,49 @@
+import base64
+
 from six.moves.urllib.parse import quote
 
 import advertools as adv
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
+import dash_bootstrap_components as dbc
+
+from dash_table import DataTable
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import pandas as pd
 import logging
+
+img_base64 = base64.b64encode(open('./logo.png', 'rb').read()).decode('ascii')
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s==%(funcName)s==%(message)s')
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+
 server = app.server
 
 app.layout = html.Div([
-    html.Br(), html.Br(),
-    html.Div('Search Engine Marketing: Keyword Generation Tool',
-             style={'text-align': 'center', 'font-size': 30}),
-    html.Br(), html.Br(),
-    html.Div([
-        html.Div([
-
-            html.Content('Edit campaign name:'),
-            dcc.Input(id='campaign_name',
-                      value='SEM_Campaign',
-                      style={'height': 35, 'width': '97%', 'font-size': 20,
-                             'font-family': 'Palatino'}),
-            html.Br(), html.Br(),
-            html.Content('Select match type(s):'),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.Img(src='data:image/png;base64,' + img_base64, width=200),
+            html.A(['online marketing', html.Br(), 'productivity & analysis'],
+                   href='https://github.com/eliasdabbas/advertools')
+        ], sm=12, lg=2, style={'text-align': 'center'}), html.Br(),
+        dbc.Col([
+            html.H2('Search Engine Marketing: Keyword Generation Tool',
+                    style={'text-align': 'center'}),
+        ], sm=12, lg=9),
+    ], style={'margin-left': '5%'}),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            dbc.Label('Edit campaign name:'),
+            dbc.Input(id='campaign_name',
+                      value='SEM_Campaign'),
+            html.Br(),
+            dbc.Label('Select match type(s):'),
             dcc.Dropdown(id='match_types',
                          multi=True,
                          options=[{'label': match, 'value': match}
@@ -37,45 +51,43 @@ app.layout = html.Div([
                                                 'Broad']],
                          value=['Exact', 'Phrase']),
             html.Br(),
-            dcc.Checklist(id='order_matters',
-                          values=[True],
-                          options=[{'label': 'Order matters', 'value': True}]),
-
-            html.Br(), html.Br(),
-            html.Div([
-                html.Div([
-                    html.Content(' Products:'),
-                    dcc.Textarea(id='products_table', value='', rows=20,
+            dbc.Checklist(id='order_matters',
+                          values=['True'],
+                          options=[{'label': 'Order matters', 'value': 'True'}]),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label(' Products:'),
+                    dbc.Textarea(id='products_table', value='', rows=20,
+                                 cols=10,
                                  placeholder='Products you sell, one per line\n'
-                                 'Example:\n\nhonda\ntoyota\nbmw\netc...',
-                                 style={'font-size': 16}),
-                ], style={'width': '30%', 'display': 'inline-block'}),
-                html.Div([
-                    html.Content(' Words:'),
-                    dcc.Textarea(id='words_table', value='', rows=20,
+                                 'Example:\n\nhonda\ntoyota\nbmw\netc...')
+                ]),
+                dbc.Col([
+                    dbc.Label(' Words:'),
+                    dbc.Textarea(id='words_table', value='', rows=20,
+                                 cols=10,
                                  placeholder='Words that signify purchase intent, '
                                  'one per line\n'
-                                 'Example:\n\nbuy\nprice\nshop\netc...',
-                                 style={'font-size': 16}),
-                ], style={'width': '30%', 'display': 'inline-block',
-                          'float': 'right'}),
+                                 'Example:\n\nbuy\nprice\nshop\netc...'),
+                ]),
             ])
-        ], style={'width': '20%', 'display': 'inline-block',
-                  'font-family': 'Palatino', 'margin-left': '2%'}),
-        html.Div([
-            html.Button(id='submit', children='Generate Keywords', n_clicks=0,
-                        style={'height': 30, 'background-color': '#C6EAFF',
-                               'border-radius': '12px', 'font-size': 18}),
+        ], sm=11, lg=3, style={'margin-left': '5%'}),
+        dbc.Col(lg=1),
+        dbc.Col([
+            dbc.Button(id='submit', children='Generate Keywords',
+                       style={'display': 'none'}),
             html.Br(), html.Br(),
-            dt.DataTable(id='output_df',
-                         editable=False,
-                         sortable=True,
-                         filterable=False,
-                         max_rows_in_viewport=10,
-                         columns=['Campaign', 'Ad Group', 'Keyword',
-                                  'Criterion Type', 'Labels'],
-                         rows=adv.kw_generate([''], [''],
-                                              match_types=['Broad']).to_dict('records')),
+            dcc.Loading(
+                DataTable(id='output_df',
+                          virtualization=True,
+                          n_fixed_rows=1,
+                          style_cell={'width': '150px'},
+                          columns=[{'name': col, 'id': col}
+                                   for col in ['Campaign', 'Ad Group',
+                                               'Keyword', 'Criterion Type',
+                                               'Labels']]),
+            ),
             html.Br(),
             html.A('Download Keywords',
                    id='download_link',
@@ -83,17 +95,15 @@ app.layout = html.Div([
                    href="",
                    target="_blank",
                    n_clicks=0),
-            html.Div(id='kw_df_summary', style={'width': '80%', 'margin-left': '5%'})
-        ], style={'width': '60%', 'display': 'inline-block', 'float': 'right',
-                  'margin-right': '5%', 'font-family': 'Palatino'}),
-    ]),
-    html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(),
+            html.Div(id='kw_df_summary')
+        ], sm=11, lg=7),
+    ] + [html.Br() for x in range(10)]),
     html.Div(id='download')
-], style={'background-color': '#eeeeee', 'font-family': 'Palatino'})
+], style={'background-color': '#eeeeee'})
 
 
 @app.callback(Output('kw_df_summary', 'children'),
-              [Input('output_df', 'rows')])
+              [Input('output_df', 'data')])
 def display_kw_df_summary(kw_df_list):
     kw_df = pd.DataFrame(kw_df_list)
     return [html.H3('Summary:'),
@@ -111,7 +121,7 @@ def display_kw_df_summary(kw_df_list):
             html.Content(' package.')]
 
 
-@app.callback(Output('output_df', 'rows'),
+@app.callback(Output('output_df', 'data'),
               [Input('submit', 'n_clicks')],
               [State('products_table', 'value'),
                State('words_table', 'value'),
@@ -120,6 +130,9 @@ def display_kw_df_summary(kw_df_list):
                State('order_matters', 'values')])
 def generate_kw_df(button, products, words, match_types, campaign_name,
                    order_matters):
+    if any([x is None for x in [button, products, words, match_types,
+                                campaign_name, order_matters]]):
+        raise PreventUpdate
     if button and products and words and match_types and campaign_name:
         logging.info(msg=locals())
 
@@ -128,15 +141,14 @@ def generate_kw_df(button, products, words, match_types, campaign_name,
         if '' in product_list:
             product_list.remove('')
         word_list = list({x.strip() for x in words.split('\n')})
-
-        return adv.kw_generate(product_list, word_list,
+        final_df = adv.kw_generate(product_list, word_list,
                                match_types=match_types,
-                               order_matters=bool(order_matters),
-                               campaign_name=campaign_name).to_dict('records')
-
+                               order_matters=bool(order_matters[0]),
+                               campaign_name=campaign_name)
+        return final_df.to_dict('rows')
 
 @app.callback(Output('download_link', 'href'),
-              [Input('output_df', 'rows')])
+              [Input('output_df', 'data')])
 def download_df(data_df):
     df = pd.DataFrame.from_dict(data_df, 'columns')
     csv_string = df.to_csv(index=False, encoding='utf-8')
@@ -151,15 +163,16 @@ def register_file_downloads(n_clicks):
         logging.info(str(n_clicks) + '_file_download')
 
 
-@app.callback(Output('submit', 'hidden'),
+@app.callback(Output('submit', 'style'),
               [Input('products_table', 'value'),
                Input('words_table', 'value'),
                Input('match_types', 'value'),
                Input('campaign_name', 'value')])
 def show_submit_button(products, words, match_types, campaign_name):
-    if products and words and match_types and campaign_name:
-        return False
-    return True
+    if products is None and words is None:
+        raise PreventUpdate
+    if products and words:
+        return {'display': 'inline'}
 
 
 if __name__ == '__main__':
